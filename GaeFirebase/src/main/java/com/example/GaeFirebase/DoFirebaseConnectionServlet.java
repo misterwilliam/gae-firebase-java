042f60a38e9cbc05b2d8e2744ad69ca507a0815d
misterwilliam <mr.williamchang@gmail.com>
2016-03-16 15:12:08 -0700

    Did a read of Firebase from GAE

--- GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
+++ GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
@@ -1,5 +1,10 @@
 package com.example.GaeFirebase;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 import java.io.IOException;
 import java.util.Properties;
 
@@ -13,7 +18,19 @@ public class DoFirebaseConnectionServlet extends HttpServlet {
       throws IOException {
     resp.setContentType("text/plain");
     resp.getWriter().println("Hello, this is a testing servlet. \n\n");
-    Properties p = System.getProperties();
-    p.list(resp.getWriter());
+
+    Firebase fbRef = new Firebase("https://crackling-torch-392.firebaseio.com/");
+    fbRef.child("channels").addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot snapshot) {
+        System.out.println(snapshot.getValue());
+      }
+
+      @Override
+      public void onCancelled(FirebaseError firebaseError) {
+        System.out.println("The read failed: " + firebaseError.getMessage());
+      }
+    });
+
   }
 }
