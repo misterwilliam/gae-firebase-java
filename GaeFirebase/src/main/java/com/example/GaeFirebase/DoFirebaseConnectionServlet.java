419cf3946fbf26e9da4ec227f185be71824e5c01
misterwilliam <mr.williamchang@gmail.com>
2016-03-17 12:14:50 -0700

    Add a hook for servlet initialization

--- GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
+++ GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
@@ -8,11 +8,18 @@ import com.firebase.client.ValueEventListener;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DoFirebaseConnectionServlet extends HttpServlet {
+
+  @Override
+  public void init() throws ServletException {
+
+  }
+
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
