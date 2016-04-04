b2bacb9f6df22983b750c03046291c53729cfe7c
misterwilliam <mr.williamchang@gmail.com>
2016-04-04 16:05:54 -0700

    Add ability read secrets from properties file

--- GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
+++ GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
@@ -5,9 +5,11 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.io.InputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +19,22 @@ public class DoFirebaseConnectionServlet extends HttpServlet {
 
   @Override
   public void init() throws ServletException {
-
+    ServletContext context = this.getServletContext();
+    try {
+      InputStream inputStream = context.getResourceAsStream(
+        "/secrets.properties");
+      if (inputStream == null) {
+        System.out.println("No file there");
+      } else {
+        Properties props = new Properties();
+        props.load(inputStream);
+        System.out.println(props.getProperty("firebaseSecret"));
+      }
+    } catch (java.net.MalformedURLException e) {
+      System.out.println("Invalid path to secrets: " + e.getMessage());
+    } catch (IOException e) {
+      System.out.println("Input stream error: " + e.getMessage());
+    }
   }
 
   @Override
@@ -26,8 +43,8 @@ public class DoFirebaseConnectionServlet extends HttpServlet {
     resp.setContentType("text/plain");
     resp.getWriter().println("Hello, this is a testing servlet. \n\n");
 
-    Firebase fbRef = new Firebase("https://crackling-torch-392.firebaseio.com/");
-    fbRef.child("channels").addValueEventListener(new ValueEventListener() {
+    Firebase fbRef = new Firebase("https://fb-channel.firebaseio.com/");
+    fbRef.child("clients").addValueEventListener(new ValueEventListener() {
       @Override
       public void onDataChange(DataSnapshot snapshot) {
         System.out.println(snapshot.getValue());
