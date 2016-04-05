384dfa5cf17ef12cddb8fd623a93ef4a5a2e745a
misterwilliam <mr.williamchang@gmail.com>
2016-04-04 18:12:12 -0700

    Adding TokenGenerator

--- GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
+++ GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
@@ -2,6 +2,8 @@ package com.example.GaeFirebase;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.servlet.ServletContext;
@@ -14,18 +16,21 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.firebase.security.token.TokenGenerator;
 
 public class DoFirebaseConnectionServlet extends HttpServlet {
 
+  private Properties props;
+
   @Override
   public void init() throws ServletException {
-    Properties props = this.getConfigProperties("/secrets.properties");
-    System.out.println(props.getProperty("firebaseSecret"));
+    this.props = this.getConfigProperties("/secrets.properties");
+    String authToken = this.getFirebaseAuthToken(this.props.getProperty("firebaseSecret"));
+    System.out.println(authToken);
   }
 
   @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     resp.setContentType("text/plain");
     resp.getWriter().println("Hello, this is a testing servlet. \n\n");
 
@@ -43,6 +48,19 @@ public class DoFirebaseConnectionServlet extends HttpServlet {
     });
   }
 
+  private String getFirebaseAuthToken(String secret) {
+    Map<String, Object> authPayload = new HashMap<String, Object>();
+    authPayload.put("uid", "gae-java-presence-listener");
+    authPayload.put("provider", "gae");
+    TokenGenerator tokenGenerator = new TokenGenerator(secret);
+    return tokenGenerator.createToken(authPayload);
+  }
+
+  private Firebase getAuthenticatedFirebaseClient(String firebaseProjectId) {
+    Firebase firebase = new Firebase("https://" + firebaseProjectId + ".firebaseio.com/");
+    return firebase;
+  }
+
   private Properties getConfigProperties(String path) {
     Properties props = new Properties();
     try {
@@ -55,7 +73,8 @@ public class DoFirebaseConnectionServlet extends HttpServlet {
     } catch (java.net.MalformedURLException e) {
       throw new RuntimeException("Invalid path: " + path);
     } catch (IOException e) {
-      throw new RuntimeException("Error reading file (likely under root /GaeFirebase/src/webapp/): " + path);
+      throw new RuntimeException(
+          "Error reading file (likely under root /GaeFirebase/src/webapp/): " + path);
     }
     return props;
   }
