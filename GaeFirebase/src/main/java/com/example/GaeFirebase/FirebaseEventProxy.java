ab86336f7ff4247194f15182755fdd1515d434a0
misterwilliam <mr.williamchang@gmail.com>
2016-04-14 15:27:07 -0700

    Forward Firebase event as json

--- GaeFirebase/src/main/java/com/example/GaeFirebase/FirebaseEventProxy.java
+++ GaeFirebase/src/main/java/com/example/GaeFirebase/FirebaseEventProxy.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +16,8 @@ import java.util.Properties;
 
 import javax.servlet.ServletContext;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.firebase.client.AuthData;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -27,7 +31,8 @@ public class FirebaseEventProxy {
   private HttpURLConnectionAuthenticator connectionAuthenticator;
   private ArrayList<URL> forwardEndpoints;
 
-  public FirebaseEventProxy(ServletContext context, Iterable<String> forwardEndpoints) throws MalformedURLException {
+  public FirebaseEventProxy(ServletContext context,
+                            Iterable<String> forwardEndpoints) throws MalformedURLException {
     this.forwardEndpoints = new ArrayList<URL>();
     for (String url : forwardEndpoints) {
       this.forwardEndpoints.add(new URL(url));
@@ -44,8 +49,14 @@ public class FirebaseEventProxy {
     this.fbRef.child("clients").addValueEventListener(new ValueEventListener() {
       @Override
       public void onDataChange(DataSnapshot snapshot) {
-        System.out.println(snapshot.getValue());
-        self.sendMessageToGaeApp("hi");
+        if (snapshot.exists()) {
+          try {
+            String json = new ObjectMapper().writeValueAsString(snapshot.getValue());
+            self.forwardToEndpoints(json);
+          } catch (JsonProcessingException e) {
+            System.out.println("Invalid JSON:" + e.getMessage());
+          }
+        }
       }
 
       @Override
@@ -55,11 +66,16 @@ public class FirebaseEventProxy {
     });
   }
   
-  private void sendMessageToGaeApp(String message) {
+  private void forwardToEndpoints(String message) {
     try {
       for (URL endpoint : this.forwardEndpoints) {
         HttpURLConnection connection = (HttpURLConnection)endpoint.openConnection();
+        connection.setRequestMethod("POST");
         this.connectionAuthenticator.authenticate(connection);
+        Map<String,Object> params = new HashMap<>();
+        params.put("data", message);
+        connection.setDoOutput(true);
+        connection.getOutputStream().write(this.encodeParams(params));
         InputStream inputStream = connection.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
@@ -70,10 +86,21 @@ public class FirebaseEventProxy {
         reader.close();
       }
     } catch (MalformedURLException e) {
-      System.out.println("Malformed URL");
+      System.out.println("Malformed URL: " + e.getMessage());
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+  
+  private byte[] encodeParams(Map<String, Object> params) throws UnsupportedEncodingException {
+    StringBuilder postData = new StringBuilder();
+    for (Map.Entry<String,Object> param : params.entrySet()) {
+        if (postData.length() != 0) postData.append('&');
+        postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+        postData.append('=');
+        postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+    }
+    return postData.toString().getBytes("UTF-8");
   }
   
   private String getFirebaseAuthToken(String secret) {
