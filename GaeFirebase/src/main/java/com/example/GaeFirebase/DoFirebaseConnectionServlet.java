dd72b6d583b802005d875d938a0e51c64c82b7a3
misterwilliam <mr.williamchang@gmail.com>
2016-04-18 11:50:37 -0700

    Got basic method for configuring dev versus prod

--- GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
+++ GaeFirebase/src/main/java/com/example/GaeFirebase/DoFirebaseConnectionServlet.java
@@ -3,12 +3,15 @@ package com.example.GaeFirebase;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.utils.SystemProperty;
 
 public class DoFirebaseConnectionServlet extends HttpServlet {
 
@@ -17,9 +20,21 @@ public class DoFirebaseConnectionServlet extends HttpServlet {
   @Override
   public void init() throws ServletException {
     try {
-      this.firebaseEventProxy = new FirebaseEventProxy(
-          Collections.singletonList(System.getProperty("GaeFireabseProxy.watch.endpoint")),
-          Collections.singletonList("http://gae-firebase-c4d3c.appspot.com/api/_presence/gae"));
+      Iterable<String> srcUrls;
+      Iterable<String> destUrls;
+      if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+        srcUrls = this.getSpaceSeparatedArgs(
+            System.getProperty("GaeFireabseProxy.prod.src.urls", ""));
+        destUrls = this.getSpaceSeparatedArgs(
+            System.getProperty("GaeFireabseProxy.prod.dest.urls", ""));
+      } else {
+        srcUrls = this.getSpaceSeparatedArgs(
+            System.getProperty("GaeFireabseProxy.dev.src.urls", ""));
+        destUrls = this.getSpaceSeparatedArgs(
+            System.getProperty("GaeFireabseProxy.dev.dest.urls", ""));
+      }
+
+      this.firebaseEventProxy = new FirebaseEventProxy(srcUrls, destUrls);
     } catch (MalformedURLException e) {
       e.printStackTrace();
     }
@@ -31,6 +46,10 @@ public class DoFirebaseConnectionServlet extends HttpServlet {
     resp.getWriter().println("Hello, this is a testing servlet. \n\n");
     
     this.firebaseEventProxy.subscribe();
+  }
+  
+  private Iterable<String> getSpaceSeparatedArgs(String args) {
+    return Arrays.asList(args.split(" "));
   }
  
 }
